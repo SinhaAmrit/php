4daa60d95e438f72333e7ae11f793ef4d767c409
SinhaAmrit <56241123+SinhaAmrit@users.noreply.github.com>
2020-09-25 17:56:12 +0530

    Create Variables.php

---
+++ Variables.php
@@ -0,0 +1,22 @@
+/**
+	* @author : Amrit Sinha
+	* Date : September 25, 2020
+	* GitHub : /SinhaAmrit
+	* CodePen : /SinhaAmrit
+	* Facebook : /codiizone
+	* Instagram : /codiizone
+	* Website : https://codiizone.blogspot.com
+	* Topic : Variables In PHP
+  **/
+<?php
+$variable = “name”;
+$literally = ‘My $variable will not print!\\n’;
+print($literally);
+$literally = “My $variable will print!\\n”;
+print($literally);
+?>
+=====================OUTPUT=====================
+This will produce following result:
+
+My name will not print!
+My name will print
